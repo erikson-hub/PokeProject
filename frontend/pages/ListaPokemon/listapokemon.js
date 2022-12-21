bf3aa9259cac84058e82a4df50b2434631a26fa0
erikson <erikson.velro@gmail.com>
2022-12-21 14:58:02 -0500

    frontend updated

--- frontend/pages/ListaPokemon/listapokemon.js
+++ frontend/pages/ListaPokemon/listapokemon.js
@@ -1,11 +1,11 @@
 // obtener datos del usuario desde el local storage
-const user = JSON.parse(localStorage.getItem("user"));
-console.log(user.name, user.email, user.username, user.list);
+const user = JSON.parse(localStorage.getItem('user'));
+console.log(user.id, user.name, user.email, user.username, user.list);
 
 let list = user.list;
 // si se agregan nuevos pokemon agregar a new_list, guardar en la base de datos con los datos de user
 
 // actualizar lista en el local storage
-user = JSON.parse(localStorage.getItem("user"));
-user.list = list;
-localStorage.setItem("user", JSON.stringify(user));
+// user = JSON.parse(localStorage.getItem('user'));
+user.list = ['test', 'tes2t'];
+localStorage.setItem('user', JSON.stringify(user));
