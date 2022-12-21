ffe1ebad44ce809530c3a1f52260b96558974528
erikson <erikson.velro@gmail.com>
2022-12-21 16:58:40 -0500

    Login update WIP

--- frontend/pages/Form/Login/login.js
+++ frontend/pages/Form/Login/login.js
@@ -1,60 +1,89 @@
 const form = document.getElementById('form');
 const btn = document.getElementById('ingresar');
-const username = document.getElementById('username');
+const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-function getUsers() {
-  fetch('http://localhost:3000/api/users')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
-console.log(getUsers());
+// function getUsers() {
+//   fetch('http://localhost:3000/api/users')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// }
+// console.log(getUsers());
 
 btn.addEventListener('click', (event) => {
   if (password.value === '') {
     console.log('el campo contraseña es obligatorio');
     event.preventDefault();
     return false;
-  } else if (username.value === '') {
+  } else if (email.value === '') {
     console.log('el campo de usuario es obligatorio');
     event.preventDefault();
     return false;
-  } else if (username.value.length > 30) {
+  } else if (email.value.length > 30) {
     console.log('El nombre del usuario es demasiado largo');
     event.preventDefault();
     return false;
   }
 });
-
+// let user = null;
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  if (password.value === 'un password en la base de datos &&') {
+  function verifyUser(email, password) {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => {
+        let user = data.find((user) => user.email === email);
+        console.log(user);
+        console.log(user.email, user.password, password);
+        if (user.password === password) {
+          console.log('test');
+          console.log(user);
+          return user;
+        }
+      });
+  }
+
+  const user = verifyUser(email.value, password.value);
+  console.log(user);
+  if (user) {
+    // actualizar objeto user en el local storage
+    user.id = id;
+    user.name = name;
+    user.email = email;
+    user.email = email;
+    user.list = list;
+    localStorage.setItem('user', JSON.stringify(user));
+    //
+    windows.location.href = '../../pages/ListaPokemons/listaPokemons.html';
+  } else {
+    alert('Usuario o contraseña incorrectos');
   }
 });
-let id = '5f9f9f9f9f9f9f9f9f9f9f9f'; // obtener de la base de datos
-let name = 'John Doe'; // obtener de la base de datos
-let email = 'doe@gmail.com';
-// let username = 'doe'; // obtener de la base de datos
-let list = [
-  'pikachu',
-  'charmander',
-  'bulbasaur',
-  'squirtle',
-  'eevee',
-  'snorlax',
-  'mew',
-  'charizard',
-  'pidgeotto',
-  'gengar',
-  'kadabra',
-]; // obtener de la base de datos
 
-// actualizar objeto user en el local storage
-/* Getting the user object from the local storage. */
-let user = JSON.parse(localStorage.getItem('user'));
-user.id = id;
-user.name = name;
-user.email = email;
-user.username = username;
-user.list = list;
-localStorage.setItem('user', JSON.stringify(user));
+// let id = '5f9f9f9f9f9f9f9f9f9f9f9f'; // obtener de la base de datos
+// let name = 'John Doe'; // obtener de la base de datos
+// let email = 'doe@gmail.com';
+// // let email = 'doe'; // obtener de la base de datos
+// let list = [
+//   'pikachu',
+//   'charmander',
+//   'bulbasaur',
+//   'squirtle',
+//   'eevee',
+//   'snorlax',
+//   'mew',
+//   'charizard',
+//   'pidgeotto',
+//   'gengar',
+//   'kadabra',
+// ]; // obtener de la base de datos
+
+// // actualizar objeto user en el local storage
+// /* Getting the user object from the local storage. */
+// let user = JSON.parse(localStorage.getItem('user'));
+// user.id = id;
+// user.name = name;
+// user.email = email;
+// user.email = email;
+// user.list = list;
+// localStorage.setItem('user', JSON.stringify(user));
